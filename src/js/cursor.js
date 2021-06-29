61823d9ea627b576943a086f3b2525d84c5900ff
XIV-XD <82765028+XIV-XD@users.noreply.github.com>
2021-06-29 21:17:30 +0530

    updated scroll indicator

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,41 +1,34 @@
 var cursor = document.querySelector(".custom-cursor");
-var follower = document.querySelector(".cursor-follower");
+var cursorr = $(".custom-cursor");
 
 document.addEventListener("mousemove", function (e) {
   cursor.style.cssText = "left: " + (e.clientX - 35) + "px; top: " + (e.clientY - 45) + "px;";
-  follower.style.cssText = "left: " + (e.clientX - 30) + "px; top: " + (e.clientY - 30) + "px;";
-});
 
-var cursorr = $(".custom-cursor"),
-  followerr = $(".cursor-follower");
-
-$(".cursor-active").on("mouseenter", function () {
-  cursorr.addClass("active");
-  followerr.addClass("active");
-});
-
-$(".cursor-active").on("mouseleave", function () {
-  cursorr.removeClass("active");
-  followerr.removeClass("active");
-});
-
-$(document).ready(function () {
-  $("#autopause").click(function () {
-    let video = document.getElementById('projVideo');
-    let isPaused = false;
-    let observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio != 1 && !video.paused) {
-          video.pause();
-          isPaused = true;
-        }
-        // else if(isPaused) {
-        //     video.play(); 
-        //     isPaused=false}
-      });
-    }, {
-      threshold: 1
-    });
-    observer.observe(video);
+  $(".cursor-active").on("mouseenter", function () {
+    cursorr.addClass("active");
   });
-})
+
+  $(".cursor-active").on("mouseleave", function () {
+    cursorr.removeClass("active");
+  });
+});
+
+
+
+
+let video = document.getElementById('projVideo');
+let isPaused = false;
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio != 1 && !video.paused) {
+      video.pause();
+      isPaused = true;
+    }
+    // else if(isPaused) {
+    //     video.play(); 
+    //     isPaused=false}
+  });
+}, {
+  threshold: 1
+});
+observer.observe(video);
